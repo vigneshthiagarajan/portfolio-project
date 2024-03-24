cbc4113a0f0a2636ae7316c315d8941f45088b3e
Vignesh Thiagarajan <vigneshthiagaraj@gmail.com>
2024-03-23 22:38:59 -0400

    Add bootstrap and other libs as static files.A

--- jobs/models.py
+++ jobs/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 # Create your models here.
 class Job(models.Model):
-    image = models.ImageField(upload_to='Images/')
+    image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=200)
